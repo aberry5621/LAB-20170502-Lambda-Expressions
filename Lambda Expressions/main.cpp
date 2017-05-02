6f18f1ffeb124402c6fd82a1bb9a6ab753e8359f
aberry5621 <aberry5621@gmail.com>
2017-05-02 09:09:38 -0700

    Initial Commit

---
+++ Lambda Expressions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lambda Expressions
+//
+//  Created by ax on 5/2/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
