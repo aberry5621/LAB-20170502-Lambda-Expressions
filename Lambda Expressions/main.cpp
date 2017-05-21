4f5e34b8a625ed7100535869d8a289221088aa59
aberry5621 <aberry5621@gmail.com>
2017-05-20 19:16:30 -0700

    Add second part of lab assignment

--- Lambda Expressions/main.cpp
+++ Lambda Expressions/main.cpp
@@ -1,3 +1,81 @@
+#define prog 1
+#if(prog == 1)
+
+/*
+ Question 1: Complete the 4 steps in the code below using appropriate STL algorithms, passing lambda expressions as operations
+ */
+
+#include <ctime>;
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <numeric> //accumulate
+#include <random>
+
+using namespace std;
+
+struct Employee {
+    int salary = 0; //let's just use whole numbers
+    int bonus = 0;
+};
+
+int main()
+
+{
+    
+    vector<Employee> myEmployees(5);
+    
+    srand(static_cast<int>(time(0)));
+    
+    int employeeID = 0;
+    
+    cout << "Enter annual sales: ";
+    
+    int sales;
+    
+    cin >> sales;
+    
+    //1. Assign random salaries, using range $50,000 to $200,000, to each of the 5 employees
+    vector<Employee>::iterator itr = myEmployees.begin();
+    // RANDOM IS	lb + rand() % (ub - lb + 1)
+    const int min = 50000;
+    const int max = 200000;
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(min, max);
+    for_each(myEmployees.begin(), myEmployees.end(), [&myEmployees, &generator, &distribution](Employee& e) {e.salary = distribution(generator);});
+    
+    //2. If annual sales exceeds $2,000,000 then assign bonuses of 10% based on each salary
+    for_each(myEmployees.begin(), myEmployees.end(), [&myEmployees, sales](Employee& e) { if (sales > 2000000) e.bonus =  e.salary * .1;});
+    
+    int sum = 0;
+    
+    if (sales > 1000000)
+        
+        //3. Display salaries and bonuses, along with employee ID: 1, 2, 3, 4, 5. Capture employeeID in the capture list
+        for_each(myEmployees.begin(), myEmployees.end(), [myEmployees, &employeeID](Employee& e){
+            cout << "Employee ID: " << ++employeeID << ", Salary: $" << e.salary << ",\t\t Bonus: $" << e.bonus << endl;
+        });
+    
+    sum = 0;
+    
+    //4. Accumulate all salaries and bonuses and display that as a single sum, Capture sum in the capture list
+    
+    for_each(myEmployees.begin(), myEmployees.end(), [&](Employee e) {sum += e.salary + e.bonus;});
+    
+    cout << "Total payments to employees: $" << sum << endl;
+    
+    cout << endl;
+    
+    return 0;
+    
+}
+
+
+
+#elif(prog == 2)
+
+
+
 /*
  
  LAMDA REFERENCE CODE
@@ -109,4 +187,4 @@ int main() {
 }
 
 
-
+#endif
